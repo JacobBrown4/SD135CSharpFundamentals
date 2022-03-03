62472859a1c93a03e0f0efd044236d81e60cac97
jacobbrown4 <jacobbrown4@gmail.com>
2022-03-03 11:57:54 -0500

    Room part duex

--- 05_Classes/Room.cs
+++ 05_Classes/Room.cs
@@ -14,17 +14,23 @@ namespace _05_Classes
         We also would like to include some constants that the define a minimum and maximum length, width, and height.
         When setting the properties, make sure to compare the values to the min/max and only set them if the value is valid.
 
-        Bonus:
+        Create constructors
         Create a method that calculates total lateral surface area.
-        Only allow the properties to be set from inside the class itself
+        Only allow the properties to be set from inside the class itself (Make all set's private, set only in constructor)
         Throw an exception if the given value is outside the permitted range.
         Test the code like we did with the Vehicle tests.
         */
 
         private const double MaxLength = 30;
         private const double MinLength = 6;
+        private const double MaxHeight = 20;
+        private const double MinHeight = 8;
+        private const double MaxWidth = 30;
+        private const double MinWidth = 6;
 
         private double _length;
+        private double _height;
+        private double _width;
 
         public double Length
         {
@@ -32,11 +38,56 @@ namespace _05_Classes
             set
             { // Compare to max and min before setting
                 // Throw an exception if outside the range
-                _length = value;
+                if (value < MinLength || value > MaxLength)
+                {
+                    throw new ArgumentException($"Outside my ranges of {MinLength} and {MaxLength}");
+                }
+                else
+                    _length = value;
+            }
+        }
+        public double Width
+        {
+            get { return _width; }
+            set
+            { // Compare to max and min before setting
+                // Throw an exception if outside the range
+                if (value >= MinWidth && value <= MaxWidth)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Outside my ranges of {MinWidth} and {MaxWidth}");
+                }
+
+                // don't store the value
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return
+                  _height;
+            }
+            set
+            { // Compare to max and min before setting
+                // Throw an exception if outside the range
+                if (value < MinHeight || value > MaxHeight)
+                {
+                    throw new ArgumentException($"Outside my ranges of {MinHeight} and {MaxHeight}");
+                }
+                else
+                    _height = value;
             }
         }
 
         // Method for square footage
+        public double CalculateSquareFootage()
+        {
+            return Length * Width;
+        }
 
     }
 }
---
+++ 05_Classes/RoomTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _05_Classes
+{
+    [TestClass]
+    public class RoomTests
+    {
+        [TestMethod]
+        public void SettingValues()
+        {
+            Room room = new Room();
+            room.Length = 800;
+            room.Width = 10;
+
+            Assert.AreEqual(80, room.CalculateSquareFootage());
+
+        }
+    }
+}
