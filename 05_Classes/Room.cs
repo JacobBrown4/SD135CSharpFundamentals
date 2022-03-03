a9782dea499904af668f9cae55309cabaa9cb859
jacobbrown4 <jacobbrown4@gmail.com>
2022-03-03 15:36:08 -0500

    Finished Room & Unit testing

--- 05_Classes/Room.cs
+++ 05_Classes/Room.cs
@@ -9,18 +9,19 @@ namespace _05_Classes
     public class Room
     {
         /*
-        Create a Room class that has three properties: one each for the length, width, and height.
-        Create a method that calculates total square footage.
-        We also would like to include some constants that the define a minimum and maximum length, width, and height.
-        When setting the properties, make sure to compare the values to the min/max and only set them if the value is valid.
+        -Create a Room class that has three properties: one each for the length, width, and height.
+        -Create a method that calculates total square footage.
+        -We also would like to include some constants that the define a minimum and maximum length, width, and height.
+        -When setting the properties, make sure to compare the values to the min/max and only set them if the value is valid.
 
-        Create constructors
-        Create a method that calculates total lateral surface area.
-        Only allow the properties to be set from inside the class itself (Make all set's private, set only in constructor)
-        Throw an exception if the given value is outside the permitted range.
-        Test the code like we did with the Vehicle tests.
+        -Create constructors
+        -Create a method that calculates total lateral surface area.
+        -Only allow the properties to be set from inside the class itself (Make all set's private, set only in constructor)
+        -Throw an exception if the given value is outside the permitted range.
+        -Test the code like we did with the Vehicle tests.
         */
-
+        // > CFCPM
+        // Constants
         private const double MaxLength = 30;
         private const double MinLength = 6;
         private const double MaxHeight = 20;
@@ -28,14 +29,25 @@ namespace _05_Classes
         private const double MaxWidth = 30;
         private const double MinWidth = 6;
 
+        // Fields
         private double _length;
         private double _height;
         private double _width;
 
+        // Constructor
+        // Room(){} Implicit, but inaccessible when another constructor exists
+        public Room(double l, double w, double h)
+        {
+            Length = l;
+            Width = w;
+            Height = h;
+        }
+
+        // Properties
         public double Length
         {
             get { return _length; }
-            set
+            private set
             { // Compare to max and min before setting
                 // Throw an exception if outside the range
                 if (value < MinLength || value > MaxLength)
@@ -49,7 +61,7 @@ namespace _05_Classes
         public double Width
         {
             get { return _width; }
-            set
+            private set
             { // Compare to max and min before setting
                 // Throw an exception if outside the range
                 if (value >= MinWidth && value <= MaxWidth)
@@ -71,7 +83,7 @@ namespace _05_Classes
                 return
                   _height;
             }
-            set
+            private set
             { // Compare to max and min before setting
                 // Throw an exception if outside the range
                 if (value < MinHeight || value > MaxHeight)
@@ -83,11 +95,29 @@ namespace _05_Classes
             }
         }
 
-        // Method for square footage
+        // Methods
         public double CalculateSquareFootage()
         {
             return Length * Width;
         }
 
+        public double CalculateTotalLateralSurfaceArea()
+        {
+            double lengthLSA = _length * _height * 2;
+            double widthLSA = _width * _height * 2;
+            return lengthLSA + widthLSA; 
+            // 2 * (height * length * width)
+        }
+
+        public double Calculate2()
+        {
+            return ((2 * Length) + (2 * Width)) * Height;
+        }
+
+        public double Calculate3()
+        {
+           return 2 * (Height * (Length + Width));
+
+        }
     }
 }
--- 05_Classes/RoomTests.cs
+++ 05_Classes/RoomTests.cs
@@ -9,12 +9,102 @@ namespace _05_Classes
         [TestMethod]
         public void SettingValues()
         {
-            Room room = new Room();
-            room.Length = 800;
-            room.Width = 10;
+            Room room = new Room(10, 12, 15);
 
-            Assert.AreEqual(80, room.CalculateSquareFootage());
+            Assert.AreEqual(120, room.CalculateSquareFootage());
 
+            Assert.AreEqual(room.CalculateTotalLateralSurfaceArea(), room.Calculate2());
+            Assert.AreEqual(room.CalculateTotalLateralSurfaceArea(), room.Calculate3());
+
+            Room room2 = new Room(10, 12, 20);
+            Assert.AreEqual(120, room2.CalculateSquareFootage());
+        }
+
+        // Unit Test Naming: Functionality_Goal
+        // Checking constructor is setting values
+        [TestMethod]
+        public void ConstructRoom_ShouldSetProperties()
+        {
+            Room room = new Room(8, 8, 10);
+
+            Assert.AreEqual(8, room.Length);
+            Assert.AreEqual(8, room.Width);
+            Assert.AreEqual(10, room.Height);
+
+        }
+
+        // Checking square footage output
+        [TestMethod]
+        public void CheckSquareFootage_ShouldReturnCorrectDouble()
+        {
+            Room room = new Room(10, 7, 10);
+            double expected = 70; // 10* 7
+            double actual = room.CalculateSquareFootage();
+
+            Assert.AreEqual(expected, actual);
+        }
+        // Checking lateral area output
+        [TestMethod]
+        public void CheckLateralSurfaceArea_ShouldReturnCorrectDouble()
+        {
+            Room room = new Room(10, 7, 10);
+            Assert.AreEqual(340, room.CalculateTotalLateralSurfaceArea());
+        }
+        // Check invalid length
+        [DataTestMethod]
+        [DataRow(4, 8, 12)] // Checking minimum length failure
+        [DataRow(40, 30, 11)] // Checking maximum length failure
+        public void CreateInvalidLength_ShouldThrowException(double l, double w, double h)
+        {
+            Exception thrownException = null;
+            try
+            {
+                // attempt this
+                Room room = new Room(l, w, h);
+            }
+            // Grab the exception
+            catch (Exception err)
+            {
+                // run this code
+                thrownException = err;
+            }
+            Assert.IsNotNull(thrownException);
+            Assert.IsInstanceOfType(thrownException, typeof(ArgumentException));
+        }
+        // Check invalid width
+        [DataTestMethod]
+        [DataRow(9, 4, 12)] // Checking minimum width failure
+        [DataRow(10, 31, 11)] // Checking maximum width failure
+        public void CreateInvalidWidth_ShouldThrowException(double l, double w, double h)
+        {
+            Exception thrownException = null;
+            try
+            {
+                // attempt this
+                Room room = new Room(l, w, h);
+            }
+            // Grab the exception
+            catch (Exception err)
+            {
+                // run this code
+                thrownException = err;
+            }
+            finally // Will always run regardless of exception thrown or not
+            {
+                Console.WriteLine("I cannot be stopped");
+                Assert.IsNotNull(thrownException);
+                Assert.IsInstanceOfType(thrownException, typeof(ArgumentException));
+            }
+            Console.WriteLine("I can be");
+        }
+        // Check invalid height
+        [DataTestMethod]
+        [DataRow(12,8,7)] // Under min
+        [DataRow(8,12,21)] // Over max
+        [ExpectedException(typeof(ArgumentException))] // I am expecting an exception and this test to fail, so it passes
+        public void CreateInvalidHeight_ShouldThrowException(double l, double w, double h)
+        {
+            Room room = new Room(l, w, h);
         }
     }
 }
