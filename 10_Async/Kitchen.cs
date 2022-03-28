6fd89bb1c1a373d2b43ed7d4d5d8455ff7a473a7
jacobbrown4 <jacobbrown4@gmail.com>
2022-03-28 16:43:24 -0400

    Async

---
+++ 10_Async/Kitchen.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Async
+{
+    public class Kitchen
+    {
+        public void PrettyPrint(string process, int color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(process);
+            Console.ResetColor();
+        }
+
+        public async Task<Fries> FryPototoesAsync(Potato potato)
+        {
+            if (potato.IsPeeled)
+            {
+                PrettyPrint("Dropping the fries in the fryer", 14);
+                await Task.Delay(5000);
+                PrettyPrint("Fries are still cooking", 14);
+                await Task.Delay(5000);
+                PrettyPrint("DING! Fries are done!", 14);
+                return new Fries(potato);
+            }
+            else
+            {
+                Console.WriteLine("This potato needs to be peeled first");
+                return null;
+            }
+        }
+
+        public Hamburger AssembleHamburger()
+        {
+            var time = 2000;
+            PrettyPrint("Assembling the hamburger", 13);
+            PrettyPrint("Setting the bun down", 4);
+            Task.Delay(time).Wait();
+            PrettyPrint("Placing the patty delicately", 12);
+            Task.Delay(time).Wait();
+            PrettyPrint("Place the cheese", 6);
+            Task.Delay(time).Wait();
+            PrettyPrint("Grab some lettuce", 10);
+            Task.Delay(time).Wait();
+            PrettyPrint("Throw down some pickles", 2);
+            Task.Delay(time).Wait();
+            PrettyPrint("Spray some Ketchup and Mustard", 12);
+            Task.Delay(time).Wait();
+            PrettyPrint("Slap the top bun", 4);
+            PrettyPrint("Congrats, Burger assembled", 13);
+            return new Hamburger();
+        }
+
+        public bool ServeMeal(Fries fries, Hamburger burger)
+        {
+            if(fries == null)
+            {
+                Console.WriteLine("The fries weren't cooked");
+                return false;
+            }
+            Console.WriteLine("You put the fries with the burger. Meal is ready!");
+            return true;
+        }
+    }
+}
---
+++ 10_Async/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to make meal");
+            Console.ReadKey();
+
+            Kitchen kitchen = new Kitchen();
+
+            Potato potato = new Potato();
+
+            // Synchronous method, single threaded
+            // Code cannot continue until finished
+            potato.Peel();
+
+            // Asynchronous, multi-threaded
+            var fries = kitchen.FryPototoesAsync(potato);
+
+            Hamburger hamburger = kitchen.AssembleHamburger();
+
+            if (!fries.IsCompleted) // Able to check the status of the Task
+            {
+                Console.WriteLine("We gotta wait for the fries!");
+            }
+            // Result demands an answer and essentially treats it as Syncrhonous from that point on.
+            kitchen.ServeMeal(fries.Result, hamburger);
+
+            Console.ReadKey();
+        }
+    }
+}
